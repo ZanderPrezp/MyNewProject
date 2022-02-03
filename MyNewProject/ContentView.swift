49ec705fbd07d9dd296d4b3be1eab0da90642f56
Alex <alex.hoch@gmx.de>
2022-02-03 14:53:16 +0100

    Initial Commit

---
+++ MyNewProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNewProject
+//
+//  Created by Alexander Hoch on 03.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNewProject/MyNewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewProjectApp.swift
+//  MyNewProject
+//
+//  Created by Alexander Hoch on 03.02.22.
+//
+
+import SwiftUI
+
+@main
+struct MyNewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
